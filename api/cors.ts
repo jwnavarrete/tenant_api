a23f4b8ef6a412f1aa48773e39759d1428ea0d46
John Navarrete <jwnavarretez@gmail.com>
2025-02-14 22:30:07 -0500

    cors updated

--- api/cors.ts
+++ api/cors.ts
@@ -1,29 +1,24 @@
 export const corsOptions = {
   origin: (origin: string | undefined, callback: Function) => {
-    // Permitir solicitudes desde "incassoapp" y cualquier subdominio de incassoapp
-    const APP_DOMAIN = process.env.APP_DOMAIN || "incassoapp.com";
+    // El dominio base para la configuración de CORS
+    const APP_DOMAIN = process.env.APP_DOMAIN || "incasso-app.com";
     
+    // Permitir solicitudes desde el dominio y subdominios
     const allowedOrigins = [
-      // Permite https://incasso-app.vercel.app:<puerto>
-      new RegExp(`^https://${APP_DOMAIN.replace('.', '\\.')}:\\d+$`),
-      // Permite https://<subdominio>.incasso-app.vercel.app:<puerto>
-      new RegExp(`^https://([a-z0-9-]+\\.)?${APP_DOMAIN.replace('.', '\\.')}:\\d+$`),
-      // Permite https://auth.incasso-app.vercel.app:443
-      new RegExp(`^https://auth\\.${APP_DOMAIN.replace('.', '\\.')}:443$`),
-      // Permite https://auth.incasso-app.vercel.app:443/
-      new RegExp(`^https://auth\\.${APP_DOMAIN.replace('.', '\\.')}:443/$`),
-      // Permite https://incasso-app.vercel.app
+      // Permite https://dominio.com
       new RegExp(`^https://${APP_DOMAIN.replace('.', '\\.')}$`),
-      // Permite https://auth.incasso-app.vercel.app
-      new RegExp(`^https://auth\\.${APP_DOMAIN.replace('.', '\\.')}$`),
+      // Permite https://subdominio.dominio.com
+      new RegExp(`^https://([a-z0-9-]+\\.)?${APP_DOMAIN.replace('.', '\\.')}$`),
     ];
 
+    // Verifica si la solicitud proviene de un origen permitido
     const isValidOrigin = allowedOrigins.some((regex) =>
       origin ? regex.test(origin) : false
     );
 
     console.log("isValidOrigin:", isValidOrigin);
 
+    // Si es un origen válido o no se especifica origen, permite la solicitud
     if (isValidOrigin || !origin) {
       callback(null, true); // Permite la solicitud
     } else {
